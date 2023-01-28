9827f085796bc1b5f0894744a2528e56ff89430a
Gallie83 <keving2913@hotmail.com>
2023-01-28 10:46:40 +0000

    Added first product with image

--- products/models.py
+++ products/models.py
@@ -1,10 +1,18 @@
 from django.db import models
 
 
+type = (
+    ('FL', 'flower'),
+    ('BQ', 'bouquet'),
+    ('IP', 'indoor_plants'),
+)
+
+
 class Product(models.Model):
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
+    type = models.CharField(max_length=2, null=True, choices=type)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(null=True, blank=True)
 
