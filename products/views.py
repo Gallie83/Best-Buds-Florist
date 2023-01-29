8078db2e339f19777192ca5dbaf59294f881a06d
Gallie83 <keving2913@hotmail.com>
2023-01-29 12:08:36 +0000

    Added product selector template with view and url

--- products/views.py
+++ products/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def products(request):
+    """ Returns Product selection page """
+
+    return render(request, 'products/products.html')
