2dc427df3b4647158a127a2b8980a104b4c9a545
Gallie83 <keving2913@hotmail.com>
2023-02-12 15:06:14 +0000

    Toasts now pop up when product added to bag

--- bag/views.py
+++ bag/views.py
@@ -21,6 +21,7 @@ def add_to_bag(request, item_id):
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
+        messages.success(request, f'Added another {product.name} to cart!')
     else:
         bag[item_id] = quantity
         messages.success(request, f'Added {product.name} to cart!')
