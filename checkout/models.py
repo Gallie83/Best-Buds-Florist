54b86c27e39a41d41288067c56fac9fe424b221d
Gallie83 <keving2913@hotmail.com>
2023-02-15 13:18:33 +0000

    Checkout models completed and migrated

--- checkout/models.py
+++ checkout/models.py
@@ -31,6 +31,18 @@ class Order(models.Model):
 
         return uuid.uuid4().hex.upper()
 
+    def update_total(self):
+        """ Calculates grand total for each line item added, with delivery cost """
+
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))[
+            'lineitem_total__sum']
+        if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
+            self.delivery_cost = 5
+        else:
+            self.delivery_cost = 0
+        self.grand_total = self.order_total + self.delivery_cost
+        self.save()
+
     def save(self, *args, **kwargs):
         """ If no order number assigned, then generate one and save """
 
@@ -38,6 +50,9 @@ class Order(models.Model):
             self.order_number = self._generate_order_number()
 
         super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.order_number
 
 
 class OrderLineItem(models.Model):
@@ -48,3 +63,14 @@ class OrderLineItem(models.Model):
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(
         max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
+
+    def save(self, *args, **kwargs):
+        """ Override original save method to set the lineitem total and
+         update the order total """
+
+        self.lineitem_total = self.product.price * self.quantity
+
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'SKU {self.product.sku} on order {self.order.order_number}'
