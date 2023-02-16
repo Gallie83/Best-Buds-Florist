3eb7cca05fba8f2dfe9da96b71e15be84ddde3e0
Gallie83 <keving2913@hotmail.com>
2023-02-16 15:17:36 +0000

    Added static css and js in checkout app with stripe

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Mc8VxBKNsKphDUQ5hCvCC4nWcODe66BYUit8e4KcCEKEnVFN4BGRFxK4inBNfY15RSz5ZBgXZutnxWSNCktnok2006ICJp4n5',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
