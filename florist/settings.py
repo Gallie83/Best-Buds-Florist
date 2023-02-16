b32c4aae826b691a83d03b8043b06bc466c5c4e9
Gallie83 <keving2913@hotmail.com>
2023-02-16 14:11:43 +0000

    Added checkout template

--- florist/settings.py
+++ florist/settings.py
@@ -48,6 +48,10 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+
+    # Others
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -61,6 +65,9 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'florist.urls'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 TEMPLATES = [
     {
@@ -78,6 +85,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
