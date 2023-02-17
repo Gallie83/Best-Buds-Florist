4e0e074e383dbdaf5035833a33e7eb93c7f55eae
Gallie83 <keving2913@hotmail.com>
2023-02-17 16:12:01 +0000

    checkout success view added with relevant url

--- checkout/views.py
+++ checkout/views.py
@@ -1,36 +1,85 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.conf import settings
 
 from .forms import OrderForm
+from .models import Order, OrderLineItem
+from products.models import Product
 from bag.contexts import bag_contents
 
 import stripe
 
 
 def checkout(request):
+    """ Returns checkout template and checks if checkout form is valid """
+
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    bag = request.session.get('bag', {})
-    if not bag:
-        messages.error(request, "There's nothing in your bag right now!")
-        return redirect(reverse('products'))
+    if request.method == 'POST':
+        bag = request.session.get('bag', {})
+        form_data = {
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+            'country': request.POST['country'],
+            'postcode': request.POST['postcode'],
+            'town_or_city': request.POST['town_or_city'],
+            'street_address1': request.POST['street_address1'],
+            'street_address2': request.POST['street_address2'],
+            'county': request.POST['county'],
+        }
+        order_form = OrderForm(form_data)
+        if order_form.is_valid():
+            order = order_form.save()
 
-    # Returns current bags grand total
-    current_bag = bag_contents(request)
-    total = current_bag['grand_total']
+            # Iterate through all items in bag
+            for item_id, quantity in bag.items():
+                try:
+                    product = Product.objects.get(id=item_id)
+                    order_line_item = OrderLineItem(
+                        order=order,
+                        product=product,
+                        quantity=quantity,
+                    )
+                    order_line_item.save()
 
-    # Converts to Integer for Stripe
-    stripe_total = round(total * 100)
+                # If product is not found, return error
+                except Product.DoesNotExist:
+                    messages.error(request, (
+                        "A product in your cart wasn't found in our database. "
+                        "Please call us for assistance!")
+                    )
+                    order.delete()
+                    return redirect(reverse('view_bag'))
 
-    stripe.api_key = stripe_secret_key
-    intent = stripe.PaymentIntent.create(
-        amount=stripe_total,
-        currency=settings.STRIPE_CURRENCY,
-    )
+            request.session['save_info'] = 'save-info' in request.POST
+            return redirect(reverse('checkout_success', args=[order.order_number]))
+        else:
+            messages.error(request, 'There was an error with your form. \
+                Please double check your information.')
 
-    order_form = OrderForm()
+    else:
+
+        bag = request.session.get('bag', {})
+        if not bag:
+            messages.error(request, "There's nothing in your cart right now!")
+            return redirect(reverse('products'))
+
+        # Returns current bags grand total
+        current_bag = bag_contents(request)
+        total = current_bag['grand_total']
+
+        # Converts to Integer for Stripe
+        stripe_total = round(total * 100)
+
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
+
+        order_form = OrderForm()
 
     if not stripe_public_key:
         messages.warning(request, 'Stripe public key not set!')
@@ -43,3 +92,23 @@ def checkout(request):
     }
 
     return render(request, template, context)
+
+
+def checkout_success(request, order_number):
+    """ Returns checkout success template if checkout form is valid """
+
+    # Save order info and return success message
+    save_info = request.session.get('save_info')
+    order = get_object_or_404(Order, order_number=order_number)
+    messages.success(
+        request, f'Order Success! Your order number is {order.number}. A confirmation email will be sent to {order.email} shortly.')
+
+    # Deletes bag information from session
+    if 'bag' in request.session:
+        del request.session['bag']
+
+    context = {
+        'order': order,
+    }
+
+    return render(request, 'checkout/checkout_success.html', context)
