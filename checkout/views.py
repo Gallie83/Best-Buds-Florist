a7720cee9f0b43787a6908b11366a264966bae94
Gallie83 <keving2913@hotmail.com>
2023-02-17 15:20:54 +0000

    checkout views has all relevant stripe info

--- checkout/views.py
+++ checkout/views.py
@@ -9,6 +9,9 @@ import stripe
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There's nothing in your bag right now!")
@@ -21,12 +24,22 @@ def checkout(request):
     # Converts to Integer for Stripe
     stripe_total = round(total * 100)
 
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Strip public key not set!')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51Mc8VxBKNsKphDUQ5hCvCC4nWcODe66BYUit8e4KcCEKEnVFN4BGRFxK4inBNfY15RSz5ZBgXZutnxWSNCktnok2006ICJp4n5',
-        'client_secret': 'test client secret',
+        'stripe_public_key': 'stripe_public_key',
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
