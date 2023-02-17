5221a7b2afea5097d4ad3655973407b1df719e4a
Gallie83 <keving2913@hotmail.com>
2023-02-17 13:11:51 +0000

    Added styling elements to checkout template

--- checkout/views.py
+++ checkout/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from bag.contexts import bag_contents
+
+import stripe
 
 
 def checkout(request):
@@ -9,6 +13,13 @@ def checkout(request):
     if not bag:
         messages.error(request, "There's nothing in your bag right now!")
         return redirect(reverse('products'))
+
+    # Returns current bags grand total
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+
+    # Converts to Integer for Stripe
+    stripe_total = round(total * 100)
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
