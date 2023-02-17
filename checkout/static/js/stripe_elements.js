a05836796ac9fad0b711ff1e5b050df92b62fff0
Gallie83 <keving2913@hotmail.com>
2023-02-17 18:07:56 +0000

    Webhook_handler.py added

--- checkout/static/js/stripe_elements.js
+++ checkout/static/js/stripe_elements.js
@@ -42,6 +42,10 @@ form.addEventListener('submit', function (ev) {
     ev.preventDefault();
     card.update({ 'disabled': true });
     $('#submit-button').attr('disabled', true);
+
+    $('#payment-form').fadeToggle(100);
+    $('#loading-overlay').fadeToggle(100);
+
     stripe.confirmCardPayment(clientSecret, {
         payment_method: {
             card: card,
@@ -55,6 +59,10 @@ form.addEventListener('submit', function (ev) {
                 </span>
                 <span>${result.error.message}</span>`;
             $(errorDiv).html(html);
+
+            $('#payment-form').fadeToggle(100);
+            $('#loading-overlay').fadeToggle(100);
+
             card.update({ 'disabled': false });
             $('#submit-button').attr('disabled', false);
         } else {
