ecad72447d91da5129c442ea9083cd5c13f36e23
Gallie83 <keving2913@hotmail.com>
2023-02-21 12:36:51 +0000

    Added forms.py to accounts app

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import UserProfile
 
 
 def account(request):
     """ Displays users account """
-    context = {}
+
+    account = get_object_or_404(UserProfile, user=request.user)
+
+    context = {'account': account, }
 
     return render(request, 'accounts/account.html', context)
