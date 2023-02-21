341f65705baf3d547ae304d9fbc2ea724c158dbd
Gallie83 <keving2913@hotmail.com>
2023-02-21 18:45:23 +0000

    Styling added to add image box

--- products/forms.py
+++ products/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Product
+from .widgets import CustomClearableFileInput
 
 
 class ProductForm(forms.ModelForm):
@@ -7,3 +8,6 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = '__all__'
+
+    image = forms.ImageField(
+        label='Image', required=False, widget=CustomClearableFileInput)
