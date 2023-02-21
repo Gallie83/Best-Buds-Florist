79f511f4ef616aa775232a1a330f8f813b295ba8
Gallie83 <keving2913@hotmail.com>
2023-02-21 12:54:37 +0000

    Added to user account template

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import UserProfile
+from .forms import UserProfileForm
 
 
 def account(request):
     """ Displays users account """
 
     account = get_object_or_404(UserProfile, user=request.user)
+    orders = account.orders.all()
 
-    context = {'account': account, }
+    form = UserProfileForm(instance=account)
+
+    context = {
+        'orders': orders,
+        'form': form,
+    }
 
     return render(request, 'accounts/account.html', context)
