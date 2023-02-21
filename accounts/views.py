69ee54582a49228a565fc5ac58dd47c5eedb944e
Gallie83 <keving2913@hotmail.com>
2023-02-21 13:55:29 +0000

    Added order_history view in accounts app

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render, get_object_or_404
-
+from django.contrib import messages
 from .models import UserProfile
 from .forms import UserProfileForm
+
+from checkout.models import Order
 
 
 def account(request):
     """ Displays users account """
 
     account = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=account)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated!')
+
     orders = account.orders.all()
 
     form = UserProfileForm(instance=account)
@@ -15,6 +24,23 @@ def account(request):
     context = {
         'orders': orders,
         'form': form,
+        'on_account_page': True,
     }
 
     return render(request, 'accounts/account.html', context)
+
+
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+        f'This is a past confirmation for order number {order_number}.'
+        'A confirmation email was sent on the day the order was made.'
+    ))
+
+    context = {
+        'order': order,
+        'from_account': True,
+    }
+
+    return render(request, 'checkout/checkout_success.html', context)
