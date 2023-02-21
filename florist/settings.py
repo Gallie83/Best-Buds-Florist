2c58ffb3f093f1b9d78edcc3412e3cfadf941a07
Gallie83 <keving2913@hotmail.com>
2023-02-21 11:17:00 +0000

    Added extra sorting logic

--- florist/settings.py
+++ florist/settings.py
@@ -22,12 +22,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = 'django-insecure-dfm_yb0$y@r5x0i@_kf7#z!z+*k%b$mta&-a_@7%5z$$2+hx9y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
-ALLOWED_HOSTS = ['best-buds.herokuapp.com', 'localhost']
+# For heoku deploy later
+# SECRET_KEY = os.environ.get('SECRET_KEY', '')
+# SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = 'DEVELOPMENT' in os.environ
+
+ALLOWED_HOSTS = ['best-buds.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
