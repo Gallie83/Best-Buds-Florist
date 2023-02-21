9cb10706ddf2428021355d3b402ddb4ae5741726
Gallie83 <keving2913@hotmail.com>
2023-02-21 09:15:01 +0000

    Added fixtures file to products

--- florist/settings.py
+++ florist/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import dj_database_url
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -129,6 +131,10 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': dj_database_url.parse('postgres://ueqginhp:XaVaKxVEE59sbyv5ndhHGajrTfR35X3z@trumpet.db.elephantsql.com/ueqginhp')
+# }
 
 
 # Password validation
