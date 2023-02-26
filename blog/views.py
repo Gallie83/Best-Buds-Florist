1d46326d2d13e4379b6de087d1c72d956fa96656
Gallie83 <keving2913@hotmail.com>
2023-02-26 12:52:59 +0000

    All posts and comments appear as expected

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import BlogPost
-
-# Create your views here.
+from .forms import CommentForm
+from django.contrib import messages
 
 
 def blog_posts(request):
@@ -17,12 +17,29 @@ def blog_posts(request):
 
 
 def post_details(request, slug):
-    """ Shows single post with all details and comments """
+    """ 
+    Shows single post with all details and comments
+    and allows user to leave comment 
+    """
 
     post = BlogPost.objects.get(slug=slug)
 
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.user_id = request.user.id
+            comment.save()
+            messages.success(request, 'Thanks for your comment!')
+            return redirect('post_details', slug=post.slug)
+
+    else:
+        form = CommentForm(request.POST)
+
     context = {
         'post': post,
+        'form': form,
     }
 
     return render(request, 'blog/blog_post.html', context)
